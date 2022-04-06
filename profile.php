9d6907a9249edb60c6084dbbd64c7eebae7363b3
Marianne <marianneneuts@gmail.com>
2022-04-06 11:13:04 +0200

    feat: add user profile page

---
+++ profile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IMD Social Showcase</title>
+</head>
+<body>
+    <?php include_once("nav.inc.php"); ?>
+
+    <a href="logout.php">Logout</a>
+</body>
+</html>
