fe18acc1b14a8ecffe335e283edc7f58b58c634b
Marianne <marianneneuts@gmail.com>
2022-04-12 19:25:37 +0200

    refactor: update navigation (display user id in url)

--- nav.inc.php
+++ nav.inc.php
@@ -1,6 +1,10 @@
 <nav class="main-nav">
+
+    <!-- logo -->
+
     <div class="nav__menu">
         <a href="index.php" class="main-nav__link">Home</a>
-        <a href="profile.php" class="main-nav__link">Profile</a>
+        <a href="profile.php?user=<?php echo($_SESSION['userId']) ?>" class="main-nav__link">Profile</a>
+        <a href="logout.php">Logout</a>
     </div>
 </nav>
