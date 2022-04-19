90337dd60957d6fb0d9f6e08f1a561f60230feaf
LiamP2000 <liampeeters@live.be>
2022-04-19 15:41:40 +0200

    feat: project upload to database

--- add_project.php
+++ add_project.php
@@ -1,5 +1,97 @@
 <?php
 
+    include_once(__DIR__ . "/classes/Db.php");
+    
+    class Project {
+        private $projectId;
+        private $title;
+        private $description;
+        private $image;
+        //private $date;
+        private $tag;
+
+        //projectId
+
+        public function setProjectId($projectId){
+            $this->projectId = $projectId;
+        }
+
+        public function getProjectId(){
+            return $this->projectId;
+        }
+
+        //title
+
+        public function setTitle($title){
+            $this->title = $title;
+        }
+
+        public function getTitle(){
+            return $this->title;
+        }
+
+        //description
+
+        public function setDescription($description){
+            $this->description = $description;
+        }
+
+        public function getDescription(){
+            return $this->description;
+        }
+
+        //image
+
+        public function setImage($image){
+            $this->image = $image;
+        }
+
+        public function getImage(){
+            return $this->image;
+        }
+
+        //tag
+
+        public function setTag($tag){
+            $this->tag = $tag;
+        }
+
+        public function getTag(){
+            return $this->tag;
+        }
+
+
+
+        public function addProject() {
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("insert into projects (title, description, image, tag) values (:title, :description, :image, :tag)");
+            $statement->bindValue(":title", $this->title);
+            $statement->bindValue(":description", $this->description);
+            $statement->bindValue(":image", $this->image);
+            $statement->bindValue(":tag", $this->tag);
+            $result = $statement->execute();
+            return $result;
+        }
+
+    }
+
+    if(!empty($_POST)) {
+        try {
+            $project = new Project();
+            $project->setTitle($_POST["title"]);
+            $project->setDescription($_POST["description"]);
+            $project->setImage($_POST["image"]);
+            $project->setTag($_POST["tag"]);
+            $project->addProject();
+
+            session_start();
+            //header("Location: login.php");
+        }
+        catch(Throwable $error) {
+            $error = $error->getMessage();
+        }
+    }
+
 
 
 ?><!DOCTYPE html>
