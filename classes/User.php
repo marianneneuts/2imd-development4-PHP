8a670297cc05a919199abb98f709b59a4ff0eb9c
Chelsea-VB <vanbreda.chelsea@skynet.be>
2022-04-21 23:10:54 +0200

    avatar

--- classes/User.php
+++ classes/User.php
@@ -6,6 +6,7 @@
         private $username;
         private $email;
         private $password;
+        private $picture;
 
         // id
         public function setUserId($userId){
@@ -179,5 +180,25 @@
             $statement->execute();
             $result = $statement->fetch();
             return $result['id'];
+        }
+
+        // get the avatar based on the user id
+        public static function getAvatarById($userId) {
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("select profile_picture from users where id = :userId");
+            $statement->bindValue(":userId", $userId);
+            $statement->execute();
+            $avatar = $statement->fetch();
+            return $avatar["profile_picture"];
+        }
+
+        // update the avatar
+        public function updateAvatar($userId, $picture){
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("update users set users.profile_picture = :picture where id = :userId");
+            $statement->bindValue(":userId", $userId);
+            $statement->bindValue(":picture", $picture);
+            $result = $statement->execute();
+            return $result;
         }
     }
