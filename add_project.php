2fe4abf8a40747797e5d44670ea0c5090054e62c
LiamP2000 <liampeeters@live.be>
2022-05-09 17:21:14 +0200

    feat: project image upload to project_pictures

--- add_project.php
+++ add_project.php
@@ -2,89 +2,6 @@
 
     include_once(__DIR__ . "/classes/Db.php");
     include_once('core/autoload.php');
-    session_start();
-    
-    
-
-    
-    class Project{
-        private $projectId;
-        private $userId;
-        private $title;
-        private $description;
-        private $image;
-        private $tag;
-        private $date;
-
-        public function getProjectId() {
-            return $this->projectId;
-        }
-
-        public function getUserId() {
-            return $this->userId;
-        }
-
-        public function getTitle() {
-            return $this->title;
-        }
-
-        public function getDescription() {
-            return $this->description;
-        }
-
-        public function getImage() {
-            return $this->image;
-        }
-
-        public function getTag() {
-            return $this->tag;
-        }
-
-        public function getDate() {
-            return $this->date;
-        }
-
-        public function setProjectId($projectId) {
-            $this->projectId = $projectId;
-        }
-
-        public function setUserId($userId) {
-            $this->userId = $userId;
-        }
-
-        public function setTitle($title) {
-            $this->title = $title;
-        }
-
-        public function setDescription($description) {
-            $this->description = $description;
-        }
-
-        public function setImage($image) {
-            $this->image = $image;
-        }
-
-        public function setTag($tag) {
-            $this->tag = $tag;
-        }
-
-        public function setDate() {
-            $date = new DateTime();
-            $this->date = $date->format('Y-m-d H:i:s');
-        }
-
-        public function save(){
-            $conn = Db::getInstance();
-            $statement = $conn->prepare("insert into projects (userId, title, description, image, tag, date) values (:userId, :title, :description, :image, :tag, :date)");
-            $statement->bindValue(":userId", $this->userId);
-            $statement->bindValue(":title", $this->title);
-            $statement->bindValue(":description", $this->description);
-            $statement->bindValue(":image", $this->image);
-            $statement->bindValue(":tag", $this->tag);
-            $statement->bindValue(":date", $this->date);
-            $statement->execute();
-        }
-    }
 
     if(!empty($_POST)){
         $project = new Project();
@@ -95,11 +12,26 @@
         }
         $project->setTitle($_POST['title']);
         $project->setDescription($_POST['description']);
-        $project->setImage($_POST['image']);
+        //$project->setImage($_POST['file']);
+
+        if(isset($_FILES['file'])) {
+            $currentDirectory = getcwd();
+            $uploadDirectory = "/project_pictures/";
+            $fileName = $_FILES['file']['name'];
+            $fileTmpName = $_FILES['file']['tmp_name'];
+            $fileSaveQuality = 80;
+            $uploadPath = $currentDirectory . $uploadDirectory . $fileName;
+            move_uploaded_file($fileTmpName, $uploadPath);
+            $project->setImage($_POST["project_pictures/" .$fileName]);
+        } else {
+            $project->setImage("project_pictures/default.png");
+        }
+
         $project->setTag($_POST['tag']);
-        //$project->setDate($_POST['date']);
         $project->save();
     }
+
+    
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -125,7 +57,7 @@
 
         <div>
             <label for="image">Image</label>
-            <input type="file" name="image" id="image">
+            <input type="file" name="file" >
         </div>
 
         <div>
@@ -134,7 +66,7 @@
         </div>
 
         <div>
-            <input type="submit" name="submitbutton" value="Submit">
+            <input type="submit" name="submit" value="Submit">
         </div>
         
     </form>
