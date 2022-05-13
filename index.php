d1d7e947bc9ac41df355f38dfc14e9649b9a8209
Chelsea-VB <vanbreda.chelsea@skynet.be>
2022-05-13 10:08:24 +0200

    style: update style profile page. feat: add cards

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php include_once('core/autoload.php'); ?>
 <?php include_once('logged_in.inc.php'); ?>
-<?php var_dump($_SESSION); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +7,12 @@
     <meta charset="UTF-8">
     <title>IMD Social Showcase</title>
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+
+ <!-- Bootstrap -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
 </head>
 <body>
     <?php include_once("nav.inc.php"); ?>
@@ -15,6 +20,15 @@
     <a href="add_project.php">
         <button>Add project</button> <!-- add css to button -->
     </a>
+
+    <div class="card" style="width: 18rem;">
+        <img src="https://dontgetserious.com/wp-content/uploads/2021/11/everything-is-fine-memes.jpeg" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
     
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -22,13 +22,13 @@
             <img src="<?php echo($avatar); ?>" alt="avatar">
         </div>
 
-        <?php if($_SESSION["userId"] === $_GET["user"]): ?>
+        <?php if($_SESSION["userId"] == $_GET["user"]): ?>
             <a class="edit_profile" href="edit_profile.php"><i class="fa fa-camera" aria-hidden="true" style="color:#fff; background:transparent;"></i></a>
         <?php endif; ?>
-    </section>
 
-    <!-- delete user profile -->
-    <a href="delete_profile.php" class="deleteUser">Delete profile</a>
+        <!-- delete user profile -->
+        <a href="delete_profile.php" class="deleteUser">Delete profile</a>
+    </section>
     
 </body>
 </html>
