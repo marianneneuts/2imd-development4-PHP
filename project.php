26db4e0712d43295707375c5e0fa08dc018a9534
LiamP2000 <liampeeters@live.be>
2022-05-26 13:04:37 +0200

    feat: content of chosen project

--- project.php
+++ project.php
@@ -4,16 +4,15 @@ include_once(__DIR__ . "/classes/Db.php");
 include_once('core/autoload.php');
 include_once('logged_in.inc.php');
 
-    $projectId = $_GET['projectID'];
+    $projectId = $_GET['projectId'];
     echo $projectId;
 //vergelijken userid uit session met userid van project 
     $project = Project::getProjectById($projectId);
     
 
-    var_dump($project);
-
 
     $projectId = $_GET['projectId'];
+    var_dump($project);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -22,9 +21,29 @@ include_once('logged_in.inc.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IMD Social Showcase</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php include_once("nav.inc.php"); ?>
+    <div >
+
+        <h1><?php echo htmlspecialchars($project['title']); ?></h1>
+        <p><?php echo htmlspecialchars($project['description']); ?></p>
+        <img src="<?php echo htmlspecialchars($project['image']); ?>" alt="">
+        <p><?php echo htmlspecialchars($project['tag']); ?></p>
+        <p><?php echo htmlspecialchars($project['date']); ?></p>
+        <br>
+        <br>
+        <a href="edit_project.php?projectId=<?php echo $project['id']; ?>">
+            <button>Edit project</button> <!-- add css to button -->
+        </a>
+
+    </div>
+
+            
+
 
     <!-- delete user project -->
     <a href="delete_project.php?projectId=<?php echo $_GET["projectId"]; ?>" class="deleteProject">Delete project</a>
